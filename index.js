180db4963882ce477ef15a975998e5f573210700
张成圆 <zhangchengyuan@biosan.cn>
2024-04-28 11:10:05 +0800

    first commit

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function calc(arr) {
   return eval(arr.join("+"));
 }
+
+//计算鱼竿
+function calcRods(target, level, gold) {
+  const total = target / (1 + 0.11 + 0.11 * 0.11)
+  return total - level * 5 * 7 - (gold - level * 5 * 400) / 600
+}
