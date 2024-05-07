6bb84cdbea5ef16301dab7f3b488bced04147580
张成圆 <zhangchengyuan@biosan.cn>
2024-05-07 16:30:28 +0800

    优化计算代码

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function calc(arr) {
 
 //计算鱼竿
 function calcRods(target, level, gold) {
-  const total = target / (1 + 0.11 + 0.11 * 0.11)
-  return total - level * 5 * 7 - (gold - level * 5 * 400) / 600
+  // const total = target / (1 + 0.11 + 0.11 * 0.11)
+  // return total - level * 5 * 7 - (gold - level * 5 * 400) / 600
+  return (600 * target - 1.11 * (7 * (900 * level - 0 + 200) + gold - 0)) / 666
 }
